3f8e45f1c18da58e7efbedbbbeb698925a60a229
Sunny Pahuja <sunny.pahuja89@gmail.com>
2021-05-12 19:04:53 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+// import App from './routerexample1';
+import App from './routerexample2';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
